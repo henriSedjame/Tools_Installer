f375560528e91b1561d6e8c4c202d66693d76c38
HSE <32457325+henriSedjame@users.noreply.github.com>
2019-09-05 19:40:57 +0200

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,16 @@
+ 
+#!/bin/bash
+
+#Installation de docker
+
+sudo apt-get purge docker lxc-docker docker-engine docker.io
+sudo apt-get install  curl  apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo systemctl status docker
+
+#Installation de docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
