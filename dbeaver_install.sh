1379e554d182192068e5bc0680c10f5435c4b10f
HSE <32457325+henriSedjame@users.noreply.github.com>
2019-09-05 21:12:54 +0200

    Create dbeaver_install.sh

---
+++ dbeaver_install.sh
@@ -0,0 +1,5 @@
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+echo "deb https://sudo apt -y  install dbeaver-cedbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+
+sudo apt update
+sudo apt -y  install dbeaver-ce
