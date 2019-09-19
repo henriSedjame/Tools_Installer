8bd260091dc58ee8215e2f261501495709634b12
HSE <32457325+henriSedjame@users.noreply.github.com>
2019-09-19 08:48:34 +0200

    Update install_docker-compose.sh

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo ' STEP #10 >>> Installation de docker compose'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
